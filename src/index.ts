10c6d78d5b69ddafb257f39b88aa72ec2b806237
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-11-17 21:00:00 +0900

    create proxy

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,19 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
+export interface Env {}
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const res = await fetch(proxy(request.url), request)
+		if (!res.ok || !res.headers.get('content-type')?.includes('text/html')) return res
+
+		return res
 	},
 };
+
+const proxy = (_url: string) => {
+	const url = new URL(_url)
+	url.protocol = 'https'
+	url.hostname = 'lifedot.jp'
+	url.port = ''
+
+	return url
+}
