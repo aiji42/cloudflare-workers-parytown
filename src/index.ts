685937a97a84981e9b7104e8a90d5e93dfd78e26
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-11-18 21:00:00 +0900

    add hono

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-export interface Env {}
+import { Hono } from 'hono'
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const res = await fetch(proxy(request.url), request)
-		if (!res.ok || !res.headers.get('content-type')?.includes('text/html')) return res
+const app = new Hono()
 
-		return res
-	},
-};
+app.all('*', async (c) => {
+	const res = await fetch(proxy(c.req.url), c.req.raw)
+	if (!res.ok || !res.headers.get('content-type')?.includes('text/html')) return res
+
+	return res
+})
+
+export default app
 
 const proxy = (_url: string) => {
 	const url = new URL(_url)
