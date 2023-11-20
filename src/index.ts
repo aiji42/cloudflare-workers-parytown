96906358341c6362b5a8022afea6cdb286ebccc2
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-11-20 21:00:00 +0900

    replace gtm to partytown

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import { Hono } from 'hono'
+import { HTMLRewriterElementContentHandlers } from '@cloudflare/workers-types/2022-08-04/index';
 
 const app = new Hono()
 
 app.get('/~partytown/:file', (c, next) => {
-	return fetch(`https://www.unpkg.com/@builder.io/partytown@0.8.1/lib/${c.req.param('file')}`)
+	console.log(c.req.param('file'))
+	return fetch(`https://www.unpkg.com/@builder.io/partytown@0.8.1/lib/${c.req.param('file')}`, { cf: { cacheEverything: true } })
 })
 
 app.all('*', async (c) => {
 	const res = await fetch(proxy(c.req.url), { method: c.req.method })
 	if (!res.ok || !res.headers.get('content-type')?.includes('text/html')) return res
 
-	return res
+	const detector = new DetectGtm()
+	await new HTMLRewriter().on(DetectGtm.selector, detector).transform(res.clone()).arrayBuffer()
+
+	return new HTMLRewriter().on(DetectGtm.selector, new DetectGtm(true)).on(GTMToPartytown.selector, new GTMToPartytown(detector.sctips)).transform(res)
 })
 
 export default app
@@ -22,4 +27,35 @@ const proxy = (_url: string) => {
 	url.port = ''
 
 	return url
+}
+
+class DetectGtm implements HTMLRewriterElementContentHandlers {
+	static selector = 'script'
+	public sctips: string[] = []
+	private remove = false
+	constructor(remove = false) {
+		this.remove = remove
+	}
+	text(element: Text): void | Promise<void> {
+		if (element.text.includes('https://www.googletagmanager.com/gtm.js')) {
+			this.sctips.push(element.text)
+			if (this.remove) element.remove()
+		}
+	}
+}
+
+class GTMToPartytown implements HTMLRewriterElementContentHandlers {
+	static selector = 'head'
+	private scripts: string[] = []
+	constructor(scripts: string[]) {
+		this.scripts = scripts
+	}
+	async element(element: Element): Promise<void> {
+		if (!this.scripts.length) return
+		const partytownScript = await fetch('https://www.unpkg.com/@builder.io/partytown@0.8.1/lib/partytown.js', { cf: { cacheEverything: true } })
+		element.prepend(`<script>${await partytownScript.text()}</script><script><script>partytown = { forward: ["dataLayer.push"] }</script></script>`, { html: true })
+		this.scripts.forEach((script) => {
+			element.append(`<script type="text/partytown">${script}</script>`, { html: true })
+		})
+	}
 }
